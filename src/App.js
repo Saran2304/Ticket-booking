b3b9999abd49567a8226dc9bc24e48d7fe585f2e
Saran2304 <sarans2304@gmail.com>
2024-09-13 16:05:53 +0530

    link 1 update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
-import {Route, Routes} from 'react-router-dom'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import Home from './Components/Home';
 import Try from './Components/Try';
 import Input from './Components/Input';
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path='/home' element={<Navigate to={'/home'}/>}/>
         <Route path='home' element={<Home/>}></Route>
         <Route path='try' element={<Try/>}></Route>
         <Route path='input' element={<Input/>}></Route>
