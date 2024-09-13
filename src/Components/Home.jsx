9ce9f0faaabd06ac8832ea6892ab808cbbcafb9a
Saran2304 <sarans2304@gmail.com>
2024-09-13 16:02:33 +0530

    new update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -131,13 +131,13 @@ export default function Home() {
                                                 </svg></span>
                                                 <p style={{ padding: '0%', margin: '0%', marginLeft: '50px' }}>Continue with Google</p>
                                             </Link><br />
-                                            <Link className="google" href="" >
+                                            <Link className="google">
                                                 <span style={{ padding: '10px', marginLeft: '8px', marginBottom: '3px' }}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                                                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
                                                 </svg></span>
                                                 <p style={{ padding: '0%', margin: '0%', marginLeft: '50px' }}>Continue with Email</p>
                                             </Link><br />
-                                            <Link className="google" href="" >
+                                            <Link className="google">
                                                 <span style={{ padding: '10px', marginLeft: '8px', marginBottom: '3px' }}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-apple" viewBox="0 0 16 16">
                                                     <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516s1.52.087 2.475-1.258.762-2.391.728-2.43m3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422s1.675-2.789 1.698-2.854-.597-.79-1.254-1.157a3.7 3.7 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56s.625 1.924 1.273 2.796c.576.984 1.34 1.667 1.659 1.899s1.219.386 1.843.067c.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758q.52-1.185.473-1.282" />
                                                     <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516s1.52.087 2.475-1.258.762-2.391.728-2.43m3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422s1.675-2.789 1.698-2.854-.597-.79-1.254-1.157a3.7 3.7 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56s.625 1.924 1.273 2.796c.576.984 1.34 1.667 1.659 1.899s1.219.386 1.843.067c.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758q.52-1.185.473-1.282" />
@@ -146,12 +146,12 @@ export default function Home() {
                                             </Link><br />
                                             <p>OR</p><br />
                                             <div className='inum'>
-                                                <img src={india} />
+                                                <img alt='' src={india} />
                                                 <p>+91</p>
                                                 <input type="text" placeholder='Continue with mobile number' className='phone' />
                                             </div><br /><br /><br /><br />
                                             <div className='agree'>
-                                                <input type="checkbox" /><p>I agree to the <a href="#"> Terms & Conditions</a> & <a href="#">Privacy Policy</a></p>
+                                                <input type="checkbox" /><p>I agree to the <Link> Terms & Conditions</Link> & <Link>Privacy Policy</Link></p>
                                             </div><br />
                                         </div>
 
@@ -184,7 +184,7 @@ export default function Home() {
                             </div>
 
                             <ul className='menu'>
-                                <Link href="#">
+                                <Link>
                                     <li style={{ display: 'flex', justifyContent: 'space-around' }}>
                                         <div className='notifi'>
                                             <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
@@ -200,7 +200,7 @@ export default function Home() {
 
                                 <hr style={{ opacity: '0.2', marginTop: '0px', marginBottom: '0px' }} />
 
-                                <Link className='nav-link disabled' style={{ opacity: '0.4' }} href="#">
+                                <Link className='nav-link disabled' style={{ opacity: '0.4' }}>
                                     <li style={{ display: 'flex', justifyContent: 'space-around' }}>
                                         <div className='notifi'>
                                             <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
@@ -220,7 +220,7 @@ export default function Home() {
 
                                 <hr style={{ opacity: '0.1', marginTop: '0px', marginBottom: '0px' }} />
 
-                                <Link className='nav-link disabled' style={{ opacity: '0.4' }} href="#">
+                                <Link className='nav-link disabled' style={{ opacity: '0.4' }}>
                                     <li style={{ display: 'flex', justifyContent: 'space-around' }}>
                                         <div className='notifi'>
                                             <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-display" viewBox="0 0 16 16">
@@ -239,7 +239,7 @@ export default function Home() {
 
                                 <hr style={{ opacity: '0.1', marginTop: '0px', marginBottom: '0px' }} />
 
-                                <Link href="#">
+                                <Link>
                                     <li style={{ display: 'flex', justifyContent: 'space-around' }}>
                                         <div className='notifi'>
                                             <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card-2-front" viewBox="0 0 16 16">
@@ -259,7 +259,7 @@ export default function Home() {
 
                                 <hr style={{ opacity: '0.2', marginTop: '0px', marginBottom: '0px' }} />
 
-                                <Link href="#">
+                                <Link>
                                     <li style={{ display: 'flex', justifyContent: 'space-around' }}>
                                         <div className='notifi'>
                                             <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-dots" viewBox="0 0 16 16">
@@ -279,7 +279,7 @@ export default function Home() {
 
                                 <hr style={{ opacity: '0.2', marginTop: '0px', marginBottom: '0px' }} />
 
-                                <Link className='nav-link disabled' style={{ opacity: '0.4' }} href="#">
+                                <Link className='nav-link disabled' style={{ opacity: '0.4' }}>
                                     <li style={{ display: 'flex', justifyContent: 'space-around' }}>
                                         <div className='notifi'>
                                             <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
@@ -299,7 +299,7 @@ export default function Home() {
 
                                 <hr style={{ opacity: '0.1', marginTop: '0px', marginBottom: '0px' }} />
 
-                                <Link href="#">
+                                <Link>
                                     <li style={{ display: 'flex', justifyContent: 'space-around' }}>
                                         <div className='notifi'>
                                             <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gift" viewBox="0 0 16 16">
@@ -318,7 +318,7 @@ export default function Home() {
 
                                 <hr style={{ opacity: '0.2', marginTop: '0px', marginBottom: '0px' }} />
 
-                                <Link href="#">
+                                <Link >
                                     <li style={{ display: 'flex', justifyContent: 'space-around' }}>
                                         <div className='notifi'>
                                             <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
@@ -344,18 +344,18 @@ export default function Home() {
             <section>
                 <div className="moviesection d-none d-lg-block overflow-hidden overflow-lg-none ">
                     <ul className='moviesul'>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Stream</a></li>
-                        <li><a href="#">Events</a></li>
-                        <li><a href="#">Plays</a></li>
-                        <li><a href="#">Sports</a></li>
-                        <li><a href="#">Activities</a></li>
+                        <li><Link>Movies</Link></li>
+                        <li><Link>Stream</Link></li>
+                        <li><Link>Events</Link></li>
+                        <li><Link>Plays</Link></li>
+                        <li><Link>Sports</Link></li>
+                        <li><Link>Activities</Link></li>
                     </ul>
                     <ul className='moviesright d-none d-lg-flex '>
-                        <li><a href="#">ListYourShow</a></li>
-                        <li><a href="#">Corporates</a></li>
-                        <li><a href="#">Offers</a></li>
-                        <li><a href="#">Gift Cards</a></li>
+                        <li><Link>ListYourShow</Link></li>
+                        <li><Link>Corporates</Link></li>
+                        <li><Link>Offers</Link></li>
+                        <li><Link>Gift Cards</Link></li>
                     </ul>
                 </div>
             </section>
